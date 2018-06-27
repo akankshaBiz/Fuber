06106261c68954998e7058167ba44caa4dbd55d8
Akanksha Rawat <akanksha27aug@gmail.com>
2018-06-27 14:54:29 +0530

    Booking api

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const CarFleetManager = require('./src/CarFleetManager');
+
+const carFleetManager = new CarFleetManager();
+
+app.post('/book', (req, res) => {
+  const { latitude, longitude, color } = req.body;
+  const trip = carFleetManager.bookCar(latitude, longitude, color);
+  const data = { tripId: trip.id, carColor: trip.car.color };
+
+  res.send(JSON.stringify(data));
+});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!'); /* eslint-disable-line no-console */
